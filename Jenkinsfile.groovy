c21435ccf939827c6c33622605278f728f682336
Shin Imai <linubshokunin@yahoo.com>
2020-07-18 22:37:16 +0100

    README

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -40,10 +40,10 @@ podTemplate(containers: [
                     sh"""
                     env
                     git remote add github https://${PAT}@github.com/shin-imai/cicd-training.git
-                    git branch ${CHANGE_BRANCH}
+                    git checkout ${CHANGE_BRANCH}
                     git branch -a
                     git remote -v
-                    #git push -u github ${CHANGE_BRANCH}
+                    git push -u github/${CHANGE_BRANCH} ${CHANGE_BRANCH}
                     """
                 }
             }
