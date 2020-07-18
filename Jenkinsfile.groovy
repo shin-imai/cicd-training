764a813d70434231fe21faebee6979140e04dafd
Shin Imai <linubshokunin@yahoo.com>
2020-07-18 22:01:11 +0100

    README

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,3 +1,4 @@
+/*
 podTemplate(containers: [
     containerTemplate(name: 'kaniko', image: 'gcr.io/kaniko-project/executor:debug', ttyEnabled: true, command: 'cat')
   ]) {
@@ -18,6 +19,7 @@ podTemplate(containers: [
         }
     }
 }
+*/
 
 podTemplate(containers: [
     containerTemplate(name: 'bjss-node', image: 'registry.default.svc.cluster.local/bjss-node:1.0.0', args: "index.js")
--- test/test.sh
+++ test/test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-WORD=$(curl http://localhost:3000)
+WORD=$(curl -fsq http://localhost:3000)
 
 if [[ "$WORD" != "Hello World" ]];then
   echo "Failed"
   exit 1
 fi
+
+exit 0
